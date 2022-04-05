c69f68e9b1a487f14645c5a0411922c50fc87647
Asochka <smana@at-j4.msk.21-school.ru>
2022-04-05 19:48:42 +0300

    path

--- pipex.c
+++ pipex.c
@@ -1,14 +1,5 @@
 #include "pipex.h"
 
-int	main(int argc, char **argv, char **envp)
-{
-	if (argc == 5 && envp)
-		ft_start_pipex(argc, argv, envp);
-	else
-		exit(EXIT_FAILURE);
-	return (0);
-}
-
 char	**ft_parsing_path(char **envp)
 {
 	char **paths;
@@ -18,19 +9,25 @@ char	**ft_parsing_path(char **envp)
 	while ((envp[i]) && (ft_strncmp(envp[i], "PATH=", 5) != 0))
 		i++;
 	envp[i] += 5;
-	paths = ft_split(envp[i], ":");
-	return (path);
-}
-
-void	ft_check_cmd(char **paths, char *cmd)
-{
-	int	i;
-
+	paths = ft_split(envp[i], ':');
 	i = 0;
-	
+	while (paths[i])
+	{
+		paths[i] = ft_strjoin(paths[i], "/");
+		i++;
+	}
+	return (paths);
 }
-void child_process()
-{
+
+// void	ft_check_cmd(char **paths, char *cmd)
+// {
+// 	int	i;
+
+// 	i = 0;
+		
+// }
+// void child_process()
+// {
 // int  i;
 // char *cmd;
 
@@ -43,10 +40,6 @@ void child_process()
 //     free(cmd) // if execve fails, we free and we try a new path
 // }
 // return (EXIT_FAILURE);
-}
-
-
-
 
 void	ft_start_pipex(int argc, char **argv, char **envp)
 {
@@ -54,38 +47,54 @@ void	ft_start_pipex(int argc, char **argv, char **envp)
 	char	**path;
 
 	path = ft_parsing_path(envp);
-	node = (t_init *)malloc(sizeof(t_init));
-	if (!node)
-		exit(EXIT_FAILURE);
-	node->fd1 = open(argv[1], O_RDONLY, 0777);
-	node->fd2 = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0777);
-	if (node->fd1 < 0 || node->fd2 < 0)
-		exit(EXIT_FAILURE);
+	// int i = -1;
+	// while (path[++i])
+	// {
+	// 	printf("%s\n", path[i]);
+	// }
+	// node = (t_init *)malloc(sizeof(t_init));
+	// if (!node)
+	// 	exit(EXIT_FAILURE);
+	// node->fd1 = open(argv[1], O_RDONLY, 0777);
+	// node->fd2 = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0777);
+	// if (node->fd1 < 0 || node->fd2 < 0)
+	// 	exit(EXIT_FAILURE);
 
-	pipex(node->fd1, node->fd2, argv[2], argv[3]);
-	return (0);
+	// pipex(node->fd1, node->fd2, argv[2], argv[3]);
+	// return (0);
 }
 
-int	pipex(int f1, int f2, char *cmd1, char *cmd2)
-{
-	int	end[2];
-	int	status;
-	pid_t	child1;
-	pid_t	child2;
+// int	pipex(int f1, int f2, char *cmd1, char *cmd2)
+// {
+// 	int	end[2];
+// 	int	status;
+// 	pid_t	child1;
+// 	pid_t	child2;
 
-	pipe(end);
-	child1 = fork();
-	if (child1 < 0)
-         return (perror("Fork: "));
-    if (child1 == 0)
-        child_one(f1, cmd1);
-    child2 = fork();
-    if (child2 < 0)
-         return (perror("Fork: "));
-    if (child2 == 0)
-        child_two(f2, cmd2);
-    close(end[0]);         // this is the parent
-    close(end[1]);         // doing nothing
-    waitpid(child1, &status, 0);  // supervising the children
-    waitpid(child2, &status, 0);  // while they finish their tasks
+// 	pipe(end);
+// 	child1 = fork();
+// 	if (child1 < 0)
+//          return (perror("Fork: "));
+//     if (child1 == 0)
+//         child_one(f1, cmd1);
+//     child2 = fork();
+//     if (child2 < 0)
+//          return (perror("Fork: "));
+//     if (child2 == 0)
+//         child_two(f2, cmd2);
+//     close(end[0]);         // this is the parent
+//     close(end[1]);         // doing nothing
+//     waitpid(child1, &status, 0);  // supervising the children
+//     waitpid(child2, &status, 0);  // while they finish their tasks
+// }
+
+
+
+int	main(int argc, char **argv, char **envp)
+{
+	if (argc == 5 && envp)
+		ft_start_pipex(argc, argv, envp);
+	else
+		exit(EXIT_FAILURE);
+	return (0);
 }
